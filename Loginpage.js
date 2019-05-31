3892f9496edebfc413776e5dbcdc0d300c576473
JenLivesToLearn <Jencancode29@gmail.com>
2019-05-30 20:41:17 -0400

    refactored forms

--- Loginpage.js
+++ Loginpage.js
@@ -9,35 +9,14 @@ import {
 import { Constants } from 'expo';
 import Button from './components/button';
 import Container from './components/container';
+import FormInput from './components/formInput';
 const loginPage = function loginPage() {
   return (
     <Container padding={50}>
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Email:</Text>
-
-        <TextInput style={styles.textInput} />
-      </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}> Password:</Text>
-        <TextInput secureTextEntry={true} style={styles.textInput} />
-      </View>
+      <FormInput label='Email'/>
+      <FormInput label='Password'/>
       <Button value='Login'/>
     </Container>
   );
 };
-const styles = StyleSheet.create({
-  textInput: {
-    borderColor: 'black',
-    borderWidth: 1,
-    flex: 2,
-  },
-  inputContainer: {
-    flexDirection: 'row',
-  },
-
-  label: {
-    flex: 1,
-  },
-});
-
 export default loginPage;
